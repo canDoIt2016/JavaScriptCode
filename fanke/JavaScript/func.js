61ec8cbfbf9edeb5191fe9a303b28211f57f09f2
canDoIt2016 <tanzhengyu1989@163.com>
2016-05-10 11:30:43 +0800

    凡客首页

---
+++ fanke/JavaScript/func.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ̷ on 2016/5/7.
+ */
+window.onload=function(){
+    var my =document.getElementsByClassName("my");
+    for(var i=0;i<my.length;i++){
+        my[i].addEventListener("mousemove",showList,false);
+        my[i].addEventListener("mouseout",removeList,false);
+    }
+
+}
+
+var showList=function(){
+    this.style.border="#A10000 solid 1px";
+    this.style.borderRadius="2px";
+    var list =this.getElementsByClassName("list");
+    list[0].style.display="block";
+}
+
+function removeList(){
+    this.style.border="#F7F7F7 solid 1px";
+    var list =this.getElementsByClassName("list");
+    list[0].style.display="none";
+}
