58e98639aed66520396116a3f32363a0236bf352
canDoIt2016 <tanzhengyu1989@163.com>
2016-05-15 23:49:30 +0800

    万年历

--- fanke/JavaScript/func.js
+++ fanke/JavaScript/func.js
@@ -2,12 +2,14 @@
  * Created by ̷ on 2016/5/7.
  */
 window.onload=function(){
-    var my =document.getElementsByClassName("my");
+    var my = document.getElementsByClassName("my");
     for(var i=0;i<my.length;i++){
         my[i].addEventListener("mousemove",showList,false);
         my[i].addEventListener("mouseout",removeList,false);
     }
-
+    var addWeCat = document.getElementsByClassName("add");
+    addWeCat[0].addEventListener("mousemove",weChatAdd,false);
+    addWeCat[0].addEventListener("mouseout",weChatRemove,false);
 }
 
 var showList=function(){
@@ -21,4 +23,22 @@ function removeList(){
     this.style.border="#F7F7F7 solid 1px";
     var list =this.getElementsByClassName("second");
     list[0].style.display="none";
+}
+
+function weChatAdd(){
+    var add=document.getElementsByClassName("add");
+    if(!document.getElementById("wechat-image")){
+        var image=document.createElement("img");
+        image.id="wechat-image";
+        image.src='./img/weixin.png';
+        image.style="position:relative;top:33px;left:-115px;"
+        add[0].appendChild(image);
+    }
+}
+function weChatRemove(){
+    var add=document.getElementsByClassName("add");
+    var weChatImage =document.getElementById("wechat-image");
+    if(weChatImage){
+        add[0].removeChild(weChatImage);
+    }
 }
